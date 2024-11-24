18a3873d706f8e95b59b5c2f260b31e7b192922f
rajea-bilal <rajea.bilal@gmail.com>
2024-11-24 11:33:54 +0000

    added eslint rules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-4xl font-bold">Crypto App</h1>
+    <div className="min-h-screen flex items-center justify-center bg-black relative overflow-hidden">
+      <div className="absolute inset-0 bg-gradient-to-b from-gray-900 via-black to-gray-900"></div>
+
+      <div className="relative z-10 text-white">
+        <h1 className="text-4xl font-bold">Crypto App</h1>
+      </div>
     </div>
   );
 }
